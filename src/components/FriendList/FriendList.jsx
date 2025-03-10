c907be0132b5e31a205b64ff71a3e78bc7d77cd7
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-03-10 17:54:57 +0100

    corr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,25 @@
 import css from '../FriendList/FriendList.module.css'
 
 export default function FriendList({friends}) {   
+    const FriendListItem = ({friends})=>{
+        return (
+       <>              
+         {
+         friends.map((friend)=>{
+                return <li key = {friend.id} className={css.friendCard}>
+                             <img src={friend.avatar} alt={friend.name} className={css.avatar} />
+                             <p className={css.name}>{friend.name}</p>
+                             <p className={friend.isOnline ? css.onLine : css.offLine}> {friend.isOnline ? "OnLine" : "Offline"}</p>
+                       </li>
+                    })
+         }
+        </>
 
+        )
+    }    
   return (
     <ul className={css.friendList}>
-           {
-                    friends.map((friend)=>{
-                        return <li key = {friend.id} className={css.friendCard}>
-                                    <img src={friend.avatar} alt={friend.name} className={css.avatar} />
-                                    <p className={css.name}>{friend.name}</p>
-                                    <p className={friend.isOnline ? css.onLine : css.offLine}> {friend.isOnline ? "OnLine" : "Offline"}</p>
-                                 </li>
-                    })
-                }
+        <FriendListItem friends = {friends}/>
     </ul>
     
   )
